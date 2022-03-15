112c1feccc40a0ad94ea411b986e07fb03061079
pome-ta <pometarou93@gmail.com>
2022-03-15 21:23:42 +0900

    fetch

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,43 +1,55 @@
 'use strict';
 
-// (function(document) {
+console.log('top');
 
-  let LightTableFilter = (function(Arr) {
+let LightTableFilter = (function (Arr) {
+  console.log(Arr);
 
-    let _input;
+  let _input;
 
-    function _onInputEvent(e) {
-      _input = e.target;
-      let tables = document.getElementsByClassName(_input.getAttribute('data-table'));
-      Arr.forEach.call(tables, function(table) {
-        Arr.forEach.call(table.tBodies, function(tbody) {
-          Arr.forEach.call(tbody.rows, _filter);
-        });
+  function _onInputEvent(e) {
+    _input = e.target;
+    let tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+    Arr.forEach.call(tables, function (table) {
+      Arr.forEach.call(table.tBodies, function (tbody) {
+        Arr.forEach.call(tbody.rows, _filter);
+      });
+    });
+  }
+
+  function _filter(row) {
+    let text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
+    row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+  }
+
+  return {
+    init: function () {
+      let inputs = document.getElementsByClassName('light-table-filter');
+      Arr.forEach.call(inputs, function (input) {
+        input.oninput = _onInputEvent;
       });
     }
+  };
+})(Array.prototype);
 
-    function _filter(row) {
-      let text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
-      row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
-    }
+document.addEventListener('readystatechange', function () {
+  console.log('readystatechange');
+  if (document.readyState === 'complete') {
+    console.log('complete');
+    LightTableFilter.init();
+  }
+});
 
-    return {
-      init: function() {
-        let inputs = document.getElementsByClassName('light-table-filter');
-        Arr.forEach.call(inputs, function(input) {
-          input.oninput = _onInputEvent;
-        });
-      }
-    };
-  })(Array.prototype);
+document.addEventListener('DOMContentLoaded', main);
+function main() {
+  console.log('DOMContentLoaded');
+}
 
-  document.addEventListener('readystatechange', function() {
-    if (document.readyState === 'complete') {
-      console.log('1');
-      LightTableFilter.init();
-    }
-  })();
-  // });
 
-// })(document);
+const data_path = new URL('./data/dummy.json', location.protocol + '//' + location.host + location.pathname).href;
 
+fetch(data_path)
+  .then(res => res.json())
+  .then(json_data => {
+    console.log(json_data);
+  });
