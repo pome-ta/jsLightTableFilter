c8b6c4a096f8ce74b5cc3b0f53ac105730d07a22
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-22 00:43:06 +0900

    add app

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,207 +1,31 @@
 'use strict';
 
 
-class myLightTableFilter {
-  constructor() {
-    this.arr = new Array();
-    this.container = null;
-    this.inputs = null;
-    this.tables = null;
-    this._createHTMLtags();
-
-  }
-  
-  init() {
-    this.inputs.oninput = this._onInputEvent;
-  }
-  
-  _onInputEvent(e) {
-    
-  }
-
-  _createHTMLtags() {
-    // xxx: ドットで繋げていけるかな？
-    // wrapper = document.createElement('div');
-    // wrapper.classList.add('wrapper');
-
-    this.inputs = document.createElement('input');
-    this.inputs.classList.add('Light-table-filter');
-    this.inputs.setAttribute('type', 'search');
-    this.inputs.setAttribute('data-table', 'order-table');
-    this.inputs.setAttribute('placeholder', '検索');
-
-    this.container = document.createElement('section');
-    this.container.classList.add('container');
-    this.container.appendChild(this.inputs);
-
-    document.body.appendChild(this.container);
-  }
-}
-
-
-new myLightTableFilter();
-
-
-let LightTableFilter = ((Arr) => {
-  let _input;
-
-  function _onInputEvent(e) {
-    _input = e.target;
-    let tables = document.getElementsByClassName(_input.getAttribute('data-table'));
-    Arr.forEach.call(tables, (table) => {
-      Arr.forEach.call(table.tBodies, (tbody) => {
-        Arr.forEach.call(tbody.rows, _filter);
-      });
-    });
-  }
-
-  function _filter(row) {
-    let text = row.textContent.toLowerCase();
-    let val = _input.value.toLowerCase();
-    row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
-  }
-
-  return {
-    init: function () {
-      let inputs = document.getElementsByClassName('light-table-filter');
-      Arr.forEach.call(inputs, (input) => {
-        input.oninput = _onInputEvent;
-      });
-    }
-  };
-  //})(Array.prototype);
-})(new Array());  // xxx: これ大丈夫？
-
-document.addEventListener('readystatechange', () => {
-  if (document.readyState === 'complete') {
-    //console.log('complete');
-    LightTableFilter.init();
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded');
 });
-
-
-
-const data_path = new URL('./data/dummy.json', location.protocol + '//' + location.host + location.pathname).href;
-
-fetch(data_path)
-  .then(res => res.json())
-  .then(json_data => {
-    //console.log('fetc');
-    let inner = convertTable(jsonDump(json_data));
-    setHTML(inner);
-
-  });
-
-
-const faviconUrl = (url) => `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}`;
 
 const dataGrid_path = new URL('./data/gridDummy.json', location.protocol + '//' + location.host + location.pathname).href;
 
-fetch(dataGrid_path)
-  .then(res => res.json())
-  .then(json_data => {
-    // const json = JSON.parse(json_data);
-    // console.log(typeof json_data);
-    // console.log(json_data);
-    const grid = jsonDump(json_data);
-    let parse = convertGrid(grid);
-    //setHTML(parse);
-  });
-
-
-
-function jsonParse(raw) {
-  let json = new Array();
-  for (let data of raw) {
-    json.push(Object.values(data));
+async function res_json(uri) {
+  const data_array = new Array();
+  const res = await fetch(uri);
+  const json_data = await res.json();
+  return JSON.parse(json_data);
+  //return json_data;
+  /*
+  for (let data of json_data) {
+    data_array.push(Object.values(data));
   }
-  return json.slice(1);
+  return data_array;
+  */
+  
+  
 }
 
-function setConvert(array) {
-  let itu = new Array();
-  for (let data of array) {
-    let fav = faviconUrl(data[1]);
-    let newdata = [fav, ...data];
-    itu.push(newdata);
-  }
-  return itu;
+let jjj = await res_json(dataGrid_path);
+
+for (let d of jjj) {
+  console.log(d);
 }
-
-
-function convertGrid(raw_json) {
-
-  let json_array = setConvert(jsonParse(raw_json));
-  let inner = '';
-  //console.log(json_array);
-  for (let data of json_array) {
-    inner += '<div class="wrapper">';
-    inner += `<div class="icon griditem"><img src="${data[0]}"></div>`;
-    inner += `<div class="title griditem">${data[1]}</div>`;
-    inner += `<div class="url griditem">${data[2]}</div>`;
-    inner += '</div>';
-  }
-  return inner;
-}
-
-
-function setHTML(inner) {
-  const ele = document.createElement('div');
-  ele.classList.add('container');
-  ele.innerHTML = inner;
-  document.body.appendChild(ele);
-}
-
-function jsonDump(raw_json) {
-  const header = Object.keys(raw_json[0]);
-  const json_array = new Array();
-  json_array.push(header)
-  // xxx: `for` で回さんでもいけるやろ 😡
-  for (let person_data of raw_json) {
-    json_array.push(Object.values(person_data));
-  }
-  return json_array;
-}
-
-// xxx: 二度手間ですけど？
-function convertTable(arr) {
-  let inner = '<table class="order-table">';
-  const header = [...arr].shift();
-  inner += get_theadTable(header);
-  const body = arr.slice(1);
-  inner += get_tbodyTable(body);
-  inner += '</table>';
-  return inner;
-}
-
-function get_theadTable(header) {
-  let inner = '<thead><tr>';
-  for (let th of header) {
-    inner += `<th>${th}</th>`;
-  }
-  inner += '</tr></thead>';
-  return inner;
-}
-
-function get_tbodyTable(body) {
-  let inner = '<tbody>';
-  for (let block of body) {
-    inner += '<tr>';
-    for (let data of block) {
-      inner += `<td>${data}</td>`;
-    }
-    inner += '</tr>';
-  }
-  inner += '</tbody>';
-  return inner;
-}
-
-
-const sampleURI = new URL('./data/sample.json', location.protocol + '//' + location.host + location.pathname).href;
-fetch(sampleURI)
-  .then(res => res.json())
-  .then(json_data => {
-    //console.log(json_data);
-    // console.log(JSON.parse(json_data));
-  });
 
---
+++ docs/js/old_app.js
@@ -0,0 +1,226 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', main);
+
+function main() {
+  console.log('DOMContentLoaded');
+}
+
+class myLightTableFilter {
+  constructor() {
+    this.arr = new Array();
+    this.container = null;
+    this.inputs = null;
+    this.tables = null;
+    this._input = null;
+    this._createHTMLtags();
+    console.log('class');
+
+  }
+  
+  init() {
+    this.inputs.oninput = this._onInputEvent;
+  }
+  
+  _onInputEvent(e) {
+    
+  }
+
+  _createHTMLtags() {
+    // xxx: ドットで繋げていけるかな？
+    // wrapper = document.createElement('div');
+    // wrapper.classList.add('wrapper');
+
+    this.inputs = document.createElement('input');
+    this.inputs.classList.add('Light-table-filter');
+    this.inputs.setAttribute('type', 'search');
+    this.inputs.setAttribute('data-table', 'order-table');
+    this.inputs.setAttribute('placeholder', '検索');
+
+    this.container = document.createElement('section');
+    this.container.classList.add('container');
+    this.container.appendChild(this.inputs);
+
+    document.body.appendChild(this.container);
+    const data_path = new URL('./data/dummy.json', location.protocol + '//' + location.host + location.pathname).href;
+    
+    fetch(data_path)
+  .then(res => res.json())
+  .then(json_data => {
+    //console.log('fetc');
+    let inner = convertTable(jsonDump(json_data));
+    setHTML(inner);
+
+  });
+
+    
+    
+  }
+}
+
+
+new myLightTableFilter();
+
+
+let LightTableFilter = ((Arr) => {
+  let _input;
+
+  function _onInputEvent(e) {
+    _input = e.target;
+    let tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+    Arr.forEach.call(tables, (table) => {
+      Arr.forEach.call(table.tBodies, (tbody) => {
+        Arr.forEach.call(tbody.rows, _filter);
+      });
+    });
+  }
+
+  function _filter(row) {
+    let text = row.textContent.toLowerCase();
+    let val = _input.value.toLowerCase();
+    row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+  }
+
+  return {
+    init: function () {
+      let inputs = document.getElementsByClassName('light-table-filter');
+      Arr.forEach.call(inputs, (input) => {
+        input.oninput = _onInputEvent;
+      });
+    }
+  };
+  //})(Array.prototype);
+})(new Array());  // xxx: これ大丈夫？
+
+document.addEventListener('readystatechange', () => {
+  if (document.readyState === 'complete') {
+    //console.log('complete');
+    LightTableFilter.init();
+  }
+});
+
+
+
+//const data_path = new URL('./data/dummy.json', location.protocol + '//' + location.host + location.pathname).href;
+/*
+fetch(data_path)
+  .then(res => res.json())
+  .then(json_data => {
+    //console.log('fetc');
+    let inner = convertTable(jsonDump(json_data));
+    setHTML(inner);
+
+  });
+
+*/
+const faviconUrl = (url) => `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}`;
+
+const dataGrid_path = new URL('./data/gridDummy.json', location.protocol + '//' + location.host + location.pathname).href;
+
+fetch(dataGrid_path)
+  .then(res => res.json())
+  .then(json_data => {
+    // const json = JSON.parse(json_data);
+    // console.log(typeof json_data);
+    // console.log(json_data);
+    const grid = jsonDump(json_data);
+    let parse = convertGrid(grid);
+    //setHTML(parse);
+  });
+
+
+
+function jsonParse(raw) {
+  let json = new Array();
+  for (let data of raw) {
+    json.push(Object.values(data));
+  }
+  return json.slice(1);
+}
+
+function setConvert(array) {
+  let itu = new Array();
+  for (let data of array) {
+    let fav = faviconUrl(data[1]);
+    let newdata = [fav, ...data];
+    itu.push(newdata);
+  }
+  return itu;
+}
+
+
+function convertGrid(raw_json) {
+
+  let json_array = setConvert(jsonParse(raw_json));
+  let inner = '';
+  //console.log(json_array);
+  for (let data of json_array) {
+    inner += '<div class="wrapper">';
+    inner += `<div class="icon griditem"><img src="${data[0]}"></div>`;
+    inner += `<div class="title griditem">${data[1]}</div>`;
+    inner += `<div class="url griditem">${data[2]}</div>`;
+    inner += '</div>';
+  }
+  return inner;
+}
+
+
+function setHTML(inner) {
+  const ele = document.createElement('div');
+  ele.classList.add('container');
+  ele.innerHTML = inner;
+  document.body.appendChild(ele);
+}
+
+function jsonDump(raw_json) {
+  const header = Object.keys(raw_json[0]);
+  const json_array = new Array();
+  json_array.push(header)
+  // xxx: `for` で回さんでもいけるやろ 😡
+  for (let person_data of raw_json) {
+    json_array.push(Object.values(person_data));
+  }
+  return json_array;
+}
+
+// xxx: 二度手間ですけど？
+function convertTable(arr) {
+  let inner = '<table class="order-table">';
+  const header = [...arr].shift();
+  inner += get_theadTable(header);
+  const body = arr.slice(1);
+  inner += get_tbodyTable(body);
+  inner += '</table>';
+  return inner;
+}
+
+function get_theadTable(header) {
+  let inner = '<thead><tr>';
+  for (let th of header) {
+    inner += `<th>${th}</th>`;
+  }
+  inner += '</tr></thead>';
+  return inner;
+}
+
+function get_tbodyTable(body) {
+  let inner = '<tbody>';
+  for (let block of body) {
+    inner += '<tr>';
+    for (let data of block) {
+      inner += `<td>${data}</td>`;
+    }
+    inner += '</tr>';
+  }
+  inner += '</tbody>';
+  return inner;
+}
+
+
+const sampleURI = new URL('./data/sample.json', location.protocol + '//' + location.host + location.pathname).href;
+fetch(sampleURI)
+  .then(res => res.json())
+  .then(json_data => {
+    //console.log(json_data);
+    // console.log(JSON.parse(json_data));
+  });
+
