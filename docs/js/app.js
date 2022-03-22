ca62c57d196dcdabb4bc6e87784e3067c77c2cc5
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-22 15:17:43 +0900

    fnc

--- docs/js/app.js
+++ docs/js/app.js
@@ -5,21 +5,23 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
 });
 
-const my_location = `${location.protocol}//${location.host}${location.pathname}`;
-const dataGrid_path = new URL('./data/gridDummy.json', my_location).href;
 
+const get_path = (path) => {
+  const _location = `${location.protocol}//${location.host}${location.pathname}`;
+  return new URL(path, _location).href;
+}
 
 const faviconUrl = (url) => `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}`;
 
 async function res_json(uri) {
   const res = await fetch(uri);
   const json_data = await res.json();
-  const json_array = [...json_data].map(data => Object.assign({'favicon': faviconUrl(data.url)}, data));
-  return json_array;
+  // xxx: 無駄が多いかしら？
+  return [...json_data].map(data => Object.assign({'favicon': faviconUrl(data.url)}, data));
 }
 
-
-
+const json_path = './data/gridDummy.json';
+const dataGrid_path = get_path(json_path);
 
 let primitive_json = await res_json(dataGrid_path);
 console.log(primitive_json);
