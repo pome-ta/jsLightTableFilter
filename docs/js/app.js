0b13f414ca5ce333ba1516780d71a19b3c616d08
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-23 01:33:49 +0900

    up

--- docs/js/app.js
+++ docs/js/app.js
@@ -28,21 +28,38 @@ const json_data = await res_json(dataGrid_path);
 // xxx: 無駄が多いかしら？
 const json_obj = [...json_data].map(data => Object.assign({'favicon': faviconUrl(data.url)}, data));
 
-//console.log(json_obj);
+
+
+function createElementAddClass(tag, name) {
+  const element_obj = document.createElement(tag);
+  element_obj.classList.add(name);
+  return element_obj;
+}
 
 function createGridElement(grid_json) {
-  const container = document.createElement('section');
-  container.classList.add('container');
-  for (let data of grid_json) {
+  const container = createElementAddClass('section', 'container');
+  document.body.appendChild(container);
+  
+  grid_json.forEach(data => {
+    for (const [key, value] of data.entries) {
+      console.log(value);
+    }
+  });
+  for (const data of grid_json) {
+    //const favicon, title, url = data;
+    //console.log(favicon);
+   // console.log(title);
+   // console.log(url);
+    
     //console.log('hoge');
     //console.log(data);
-    let wrap = document.createElement('div');
+    const wrap = document.createElement('div');
     wrap.classList.add('wrap');
     wrap.innerHTML = Object.values(data);
     container.appendChild(wrap);
   }
   
-  document.body.appendChild(container);
+  
   
 }
 
