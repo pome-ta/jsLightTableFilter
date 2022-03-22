69618705d002af8d0ff0e10978056244603b26a8
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-22 14:33:08 +0900

    do not

--- docs/js/app.js
+++ docs/js/app.js
@@ -5,27 +5,34 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
 });
 
-const dataGrid_path = new URL('./data/gridDummy.json', location.protocol + '//' + location.host + location.pathname).href;
+//const dataGrid_path = new URL('./data/gridDummy.json', location.protocol + '//' + location.host + location.pathname).href;
+// todo: あまりかわらんか
+const dataGrid_path = new URL('./data/gridDummy.json', `${location.protocol}//${location.host}${location.pathname}`).href;
+const faviconUrl = (url) => `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}`;
 
 async function res_json(uri) {
   const data_array = new Array();
   const res = await fetch(uri);
   const json_data = await res.json();
-  return JSON.parse(json_data);
   //return json_data;
-  /*
+  //const json_array = [...json_data].map(data => {{'favicon': faviconUrl(data.url)}, ...data});
+  
+  const json_array = [...json_data].map((data, index, array) => console.log(data, index, array));
+  
+  console.log(json_array);
   for (let data of json_data) {
-    data_array.push(Object.values(data));
+    //console.log(data.url);
+    let favicon = {'favicon': faviconUrl(data.url)};
+    //data_array.push({favicon, ...data})
+    //console.log({favicon, ...data});
+    data.favicon = favicon;
   }
-  return data_array;
-  */
-  
-  
+  //console.log(data_array);
+  //console.log(json_data);
 }
 
-let jjj = await res_json(dataGrid_path);
 
-for (let d of jjj) {
-  console.log(d);
-}
+
+
+let primitive_json = await res_json(dataGrid_path);
 
