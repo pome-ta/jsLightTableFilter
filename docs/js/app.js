ae7168a16841514fb27881a10024f8e12163c87b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-22 18:48:33 +0900

    createElement

--- docs/js/app.js
+++ docs/js/app.js
@@ -16,13 +16,34 @@ const faviconUrl = (url) => `https://www.google.com/s2/favicons?domain=${new URL
 async function res_json(uri) {
   const res = await fetch(uri);
   const json_data = await res.json();
-  // xxx: 無駄が多いかしら？
-  return [...json_data].map(data => Object.assign({'favicon': faviconUrl(data.url)}, data));
+  return json_data;
 }
+
+
 
 const json_path = './data/gridDummy.json';
 const dataGrid_path = get_path(json_path);
 
-let primitive_json = await res_json(dataGrid_path);
-console.log(primitive_json);
+const json_data = await res_json(dataGrid_path);
+// xxx: 無駄が多いかしら？
+const json_obj = [...json_data].map(data => Object.assign({'favicon': faviconUrl(data.url)}, data));
 
+//console.log(json_obj);
+
+function createGridElement(grid_json) {
+  const container = document.createElement('section');
+  container.classList.add('container');
+  for (let data of grid_json) {
+    //console.log('hoge');
+    //console.log(data);
+    let wrap = document.createElement('div');
+    wrap.classList.add('wrap');
+    wrap.innerHTML = Object.values(data);
+    container.appendChild(wrap);
+  }
+  
+  document.body.appendChild(container);
+  
+}
+
+createGridElement(json_obj);
